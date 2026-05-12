eb9764c84017f35e38d401fe6776a3ea8396cc4b
Shreya Jamwal <shreyajamwal19@gmail.com>
2026-05-12 15:38:33 +0530

    Add Netlify configuration and cleanup root directory

--- src/main/java/com/rosette/boutique/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rosette.boutique.repositories;
-
-import com.rosette.boutique.models.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findByEmail(String email);
-}
